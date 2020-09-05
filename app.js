cb92f1db418e8ab9c4cb31bb45021203c5fc28ab
Tim Williamson <timothy.williamson@santander.co.uk>
2020-09-05 16:55:08 +0100

    add users route skeleton

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var userRouter = require('./routes/users');
 var bloggsRouter = require('./routes/bloggs');
 
 var app = express();
@@ -18,6 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/bloggs', bloggsRouter);
 
 // catch 404 and forward to error handler

e1d9ffb50648b397b27d48abc657f2f3f3924474
Tim Williamson <timothy.williamson@santander.co.uk>
2020-10-01 12:16:41 +0100

    refactor users-service - extract private method to check whether username already taken

--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -7,29 +7,33 @@ export class UsersService {
     async createUser(username: string, password: string) {
         await this.datamapper.ensureTableExists(User, {readCapacityUnits: 5, writeCapacityUnits: 5});
 
-        const users = []
-        for await ( let user of this.datamapper.query(User, {username: username})) {
-            users.push(user);
-        };
-
-        if (users.length > 0) {
-            throw createError(409, 'Username already taken')
-        }
+        await this.checkUsernameNotTaken(username);
 
         const user = await User.create(username, password)
 
         await this.datamapper.put(user);
     }
-
+    
     async login(username: string, password: string) {
         try {
             const user: User = await this.datamapper.get(User, {username: username});
-
+            
             if (user.authenticate(password)) {
                 return user;
             }
         } catch  {}
         
         throw createError(401, 'Authorisation failed');
+    }
+
+    private async checkUsernameNotTaken(username: string) {
+        const users = [];
+        for await (let user of this.datamapper.query(User, { username: username })) {
+            users.push(user);
+        }
+
+        if (users.length > 0) {
+            throw createError(409, 'Username already taken');
+        }
     }
 }

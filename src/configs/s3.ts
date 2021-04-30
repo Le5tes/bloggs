5389b5b73f4590ca358852c8db08944ddf61ff6e
Tim Williamson <timothy.williamson@santander.co.uk>
2021-04-30 14:16:57 +0100

    chore(s3) try adding creds again

--- src/configs/s3.ts
+++ src/configs/s3.ts
@@ -3,7 +3,11 @@ import { mockS3 } from "../utils/mock-s3-presigned";
 export const getS3 = () => {
     if (process.env.PRODENV ) {
         var AWS = require('aws-sdk');
-        AWS.config.update({region: process.env.REGION});
+        var credentials = {
+            accessKeyId: process.env.S3_ACCESS_KEY,
+            secretAccessKey : process.env.S3_SECRET_KEY
+        };
+        AWS.config.update({credentials, region: process.env.REGION});
         return new AWS.S3();
     } else {
         return mockS3

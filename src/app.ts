52c56409fc153cbece0c937c3110292acaa07793
Tim Williamson <timothy.williamson@santander.co.uk>
2021-05-27 19:28:12 +0100

    fix(app.ts) set credentials true for cors

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,10 @@ export const getApp = async () => {
   var app = express();
 
   // TODO set some cors
-  app.use(cors({origin: /lestes\.tech/}))
+  app.use(cors({
+    origin: /lestes\.tech/,
+    credentials: true
+  }))
 
   app.use(logger('dev'));
   app.use(express.json());

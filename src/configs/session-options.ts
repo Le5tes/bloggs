1d76f706c8184c6d4c884f8c0809deb1d6f2b0a2
Tim Williamson <timothy.williamson@santander.co.uk>
2021-05-27 09:09:11 +0100

    chore(session-options) add cookie domain

--- src/configs/session-options.ts
+++ src/configs/session-options.ts
@@ -3,5 +3,6 @@ var DynamoDBStore = require('connect-dynamodb')(session);
 
 export const sessionOptions = process.env.PRODENV ? {
     store: new DynamoDBStore(),
-    secret: process.env.sessionSecret
+    secret: process.env.sessionSecret,
+    cookie: { domain: process.env.domain }
 } : { secret: 'keyboard cat'};
